407583f9370dc00540947394119e07aac5024b1e
unknown <jpsd.pedrosoll@hotmail.com>
2023-01-16 18:36:52 -0300

    Adição das dependencias da aula 5

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
 
 const clienteRoute = require("./routes/clienteRoute");
 const crudRoute = require("./routes/crudRoute");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //dirname significa pasta do diretório ou pasta em que os arquivos estão 
 app.set("view engine", "ejs");
 
 app.use("/", clienteRoute);
