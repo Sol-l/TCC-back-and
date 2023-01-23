e63472d266070e0f8650e7de47caeff2356cc339
Sol-l <jpsd.pedrosoll@hotmail.com>
2023-01-23 17:41:43 -0300

    ícone eye adicionado e funcionando

---
+++ public/cliente/js/script.js
@@ -0,0 +1,9 @@
+function mostrarOcultarSenha(){
+    var senha=document.getElementById("PasswordC");
+
+    if(senha.type=="password"){
+        senha.type="text";
+    }else{
+        senha.type="password";
+    }
+}
