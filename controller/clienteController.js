4c710ae39b2b223b98bdd2781f65cf0bb5905e4c
unknown <jpsd.pedrosoll@hotmail.com>
2023-03-08 11:31:03 -0300

    funfando

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -6,8 +6,19 @@ async function index(req, res) {
   if(typeof req.user !== 'undefined'){
     usuario = await Usuario.findById(req.user.id)
   }
+  const Tábua = await Produto.find({categoria:"Tábua"})
+  const Faca = await Produto.find({categoria:"Faca"})
+  const Colar = await Produto.find({categoria:"Colar"})
+  const Quadro = await Produto.find({categoria:"Quadro"})
+  const Abajur = await Produto.find({categoria:"Abajur"})
+  const Mandala = await Produto.find({categoria:"Mandala"})
+  const Rústico = await Produto.find({categoria:"Rústico"})
+  const Cuia = await Produto.find({categoria:"Cuia"})
+  const Térmica = await Produto.find({categoria:"Térmica"})
   const produtos = await Produto.find()
-  res.render("cliente/index.ejs",{Admin:usuario, Produtos:produtos});
+  res.render("cliente/index.ejs",{
+  Admin:usuario, Tábua:Tábua, Faca:Faca, Colar:Colar, Quadro:Quadro, Abajur:Abajur, Mandala:Mandala,
+  Rústico:Rústico, Cuia:Cuia, Térmica:Térmica, Produtos:produtos});
 }
 
 async function indexLogout(req, res) {
@@ -46,8 +57,19 @@ async function abrestore(req, res) {
   if(typeof req.user !== 'undefined'){
     usuario = await Usuario.findById(req.user.id)
   }
+  const Tábua = await Produto.find({categoria:"Tábua"})
+  const Faca = await Produto.find({categoria:"Faca"})
+  const Colar = await Produto.find({categoria:"Colar"})
+  const Quadro = await Produto.find({categoria:"Quadro"})
+  const Abajur = await Produto.find({categoria:"Abajur"})
+  const Mandala = await Produto.find({categoria:"Mandala"})
+  const Rústico = await Produto.find({categoria:"Rústico"})
+  const Cuia = await Produto.find({categoria:"Cuia"})
+  const Térmica = await Produto.find({categoria:"Térmica"})
   const produtos = await Produto.find()
-  res.render("cliente/store.ejs",{Admin:usuario, Produtos:produtos});
+  res.render("cliente/store.ejs",{
+  Admin:usuario, Tábua:Tábua, Faca:Faca, Colar:Colar, Quadro:Quadro, Abajur:Abajur, Mandala:Mandala,
+  Rústico:Rústico, Cuia:Cuia, Térmica:Térmica, Produtos:produtos});
 }
 
 async function abrecheckout(req, res) {
