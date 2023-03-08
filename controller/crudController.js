9ee55f46a11f966e0c1180f616f9b3303696d089
joão pedro soll dias <jpsd.pedrosoll@hotmail.com>
2023-03-08 19:47:55 -0300

    Página de excluir produtos funcionando

--- controller/crudController.js
+++ controller/crudController.js
@@ -1,9 +1,17 @@
+var Produto = require('../model/produto')
+
 async function adicionaproduto(req, res) {
   res.render("crud/adicionaproduto.ejs");
 }
 
 async function excluiproduto(req, res) {
-  res.render("crud/excluiproduto.ejs");
+  let usuario = ''
+  if(typeof req.user !== 'undefined'){
+    usuario = await Usuario.findById(req.user.id)
+  }
+  const produtos = await Produto.find()
+  res.render("crud/excluiproduto.ejs",{
+  Admin:usuario, Produtos:produtos});
 }
 
 module.exports = {
